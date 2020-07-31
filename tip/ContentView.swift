ce866f0dd9d4e8e75944c06b626c51052b1a22b9
jonathan leibovici <jonathanleibovici@gmail.com>
2020-07-30 23:35:56 -0400

    inital commit

---
+++ tip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tip
+//
+//  Created by Jonathan Leibovici on 7/29/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
